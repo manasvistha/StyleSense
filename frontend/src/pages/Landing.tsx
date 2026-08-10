dfa68c4137c1e4881fcd9fa0a4185fc5cf8145c8
Mansvi Shrestha <manasvishrestha6@gmail.com>
2026-08-10 19:47:09 +0545

    changed in homepage

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -31,7 +31,7 @@ const fade = {
 
 export default function Landing() {
   return (
-    <div className="overflow-hidden">
+    <div className="overflow-hidden bg-[#FFECF0]">
       <Hero />
       <TrustBar />
       <HowItWorks />
